71ccc214e6b34fee14c27a218ffeab71802c7a70
wuyaowei <wuyaowei1989@163.com>
2018-01-31 23:08:55 +0800

    ai 接口

--- app/src/main/java/com/android/funny/ui/imageclassify/ImageClassifyFragment.java
+++ app/src/main/java/com/android/funny/ui/imageclassify/ImageClassifyFragment.java
@@ -1,6 +1,13 @@
 package com.android.funny.ui.imageclassify;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,16 +24,21 @@ import com.android.funny.component.DaggerHttpComponent;
 import com.android.funny.ui.base.BaseFragment;
 import com.android.funny.ui.imageclassify.contract.ImageClassifyContract;
 import com.android.funny.ui.imageclassify.presenter.ImageClassifyPresenter;
+import com.android.funny.utils.BitmapUtils;
 import com.baidu.aip.imageclassify.AipImageClassify;
+import com.tbruyelle.rxpermissions2.RxPermissions;
 
 import org.json.JSONObject;
 
+import java.io.File;
 import java.util.HashMap;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
 
 /**
  * Created by Administrator on 2018/1/31.
@@ -44,6 +56,12 @@ public class ImageClassifyFragment extends BaseFragment<ImageClassifyPresenter> 
     @BindView(R.id.image_tv)
     TextView imageTv;
     Unbinder unbinder;
+
+    private static final int PICK_REQUEST_CODE = 0;
+    private static final int CAMERA_REQUEST_CODE = 1;
+    private static final int ZOOM_REQUEST_CODE = 2;
+
+    private static final String IMAGE_FILE_NAME = "photo.jpg";
 
     public static ImageClassifyFragment newInstance() {
         Bundle args = new Bundle();
@@ -103,6 +121,123 @@ public class ImageClassifyFragment extends BaseFragment<ImageClassifyPresenter> 
                 break;
             case R.id.upload_img_tv:
                 break;
+        }
+    }
+
+    private void takePhoto() {
+        RxPermissions rxPermissions = new RxPermissions(getActivity());
+        rxPermissions.request(Manifest.permission.CAMERA).subscribe(new Consumer<Boolean>() {
+            @Override
+            public void accept(@NonNull Boolean aBoolean) throws Exception {
+                if (aBoolean) {
+                    //相机
+                    if (BitmapUtils.isSdcardExisting()) {
+                        Intent intent_camera = new Intent(
+                                "android.media.action.IMAGE_CAPTURE");
+                        intent_camera.putExtra(MediaStore.EXTRA_OUTPUT, getImageUri());
+                        intent_camera.putExtra(
+                                MediaStore.EXTRA_VIDEO_QUALITY, 0);
+                        startActivityForResult(intent_camera,
+                                CAMERA_REQUEST_CODE);
+
+                    } else {
+                        Toast.makeText(getActivity(), "请插入sd卡",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    //未获取权限
+                    Toast.makeText(getContext(), "您没有授权该权限，请在设置中打开授权", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void photoAlbum() {
+        RxPermissions rxPermissions = new RxPermissions(getActivity());
+        rxPermissions.request(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE).subscribe(new Consumer<Boolean>() {
+            @Override
+            public void accept(Boolean aBoolean) throws Exception {
+                if (aBoolean) {
+                    //从相册选择
+                    Intent intent_gallery = new Intent(
+                            Intent.ACTION_PICK,
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent_gallery
+                            .setDataAndType(
+                                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                    "image/*");
+                    ImageClassifyFragment.this.startActivityForResult(intent_gallery,
+                            PICK_REQUEST_CODE);
+                } else {
+                    //未获取权限
+                    Toast.makeText(ImageClassifyFragment.this.getContext(), "您没有授权该权限，请在设置中打开授权", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    //获取图片的uri
+    private Uri getImageUri() {
+        return Uri.fromFile(new File(Environment.getExternalStorageDirectory(),
+                IMAGE_FILE_NAME));
+    }
+
+    // 在从拍照或者图片选择后返回
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        } else {
+            switch (requestCode) {
+                case PICK_REQUEST_CODE:
+                    // 从图片选择图片后，直接返回的是uri，然后对通过uri取得图片，进行剪切；
+                    startPhotoZoom(data.getData());
+                    break;
+                case CAMERA_REQUEST_CODE:
+                    // 首先判断sd卡是否挂载；
+                    if (BitmapUtils.isSdcardExisting()) {
+                        startPhotoZoom(getImageUri());
+                    } else {
+                        Toast.makeText(getActivity(), "未找到存储卡，无法存储照片！",
+                                Toast.LENGTH_LONG).show();
+                    }
+                    break;
+                // 启动剪切图片；
+                case ZOOM_REQUEST_CODE:
+                    if (data != null) {
+                        showImage(data);
+                    }
+                    break;
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    // 对图片进行剪切；
+    public void startPhotoZoom(Uri uri) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        intent.putExtra("crop", "true");
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("outputX", 360);
+        intent.putExtra("outputY", 360);
+        //intent.putExtra("circleCrop", true);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, ZOOM_REQUEST_CODE);
+    }
+
+    // 最张显示图片；但是在实际中，在进入activity中要判断sd卡中是否有图片，如果有图片要首先显示图片。
+    // 把图片保存到本地，并且上传给服务器；
+    private void showImage(Intent data) {
+        Bundle extras = data.getExtras();
+        if (extras != null) {
+            Bitmap photo = extras.getParcelable("data");
+            Bitmap photo_round = BitmapUtils.toRoundBitmap(photo);
+            // saveServer();保存到服务器；
+            // saveSD();保存到sd卡;
+            BitmapUtils.saveToSDBitmap(getActivity(), "zmIcon.png",
+                    photo_round);
         }
     }
 
